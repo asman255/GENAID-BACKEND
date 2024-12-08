6cba58382ffc4954f3bf5162bb926c0515e55558
asman255 <smandth@gmail.com>
2024-12-09 01:43:38 +0700

    com to pull

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ import "dotenv/config";
 
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 // connectDB();
 // connectCloudinary();
 
-app.use(express.json());
+// app.use(express.json());
 
 // const allowedOrigins = [
 //   "https://frontend-admin-psi-green.vercel.app",
@@ -46,10 +43,16 @@ app.use(express.json());
 // app.use("/api/cart", cartRouter);
 // app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.status(200).send("API is Working");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("API is Working");
+// });
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
-});
+}
+);
